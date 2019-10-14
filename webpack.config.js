2ee829dad31c1899549ed840cb7965557e7d8767
creativeof <young_persons_town@yahoo.co.jp>
2019-10-14 20:22:07 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path')
 
-// resolve 絶対パスを生成するメソッド
-// __dirname 現在のディレクトリ
 const outputPath = path.resolve(__dirname, 'dist')
-// console.log(outputPath)
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: outputPath
+  },
+  devServer: {
+    contentBase: outputPath
   }
 }

1d708a799048e270c4686f0ddc968736e753b9ec
creativeof <young_persons_town@yahoo.co.jp>
2019-10-14 21:26:39 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import _ from 'lodash'
+// import { NAME, NiJou } from './utilities'
+// import { NAME as NAME_OF_HAM } from './utilities'
+// import * as utilities from './utilities'
+import Tiger from './utilities'
+
+// console.log(NiJou(3));
+// console.log(NAME);
+// console.log(NAME_OF_HAM);
+// console.log(utilities.NAME);
+console.log(Tiger.say());
 
 function component() {
     const element = document.createElement('div');
---
+++ src/utilities.js
@@ -0,0 +1,12 @@
+export function NiJou(num) {
+  return num ** 2;
+}
+// console.log(NiJou(2));
+
+export const NAME = 'Ham'
+
+export default class Lion {
+  static say() {
+    return 'Roar'
+  }
+}
